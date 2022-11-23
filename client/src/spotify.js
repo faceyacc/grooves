f85166c6298797aaa1181a008e3ac6bfa5162375
Ty <justfacey@gmail.com>
2022-11-22 19:05:18 -0800

    fixed logout flow

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,76 +1,84 @@
 import axios from 'axios';
 
-// Map for localStorage key
+// Map for localStorage keys
 const LOCALSTORAGE_KEYS = {
     accessToken: 'spotify_access_token',
     refreshToken: 'spotify_refresh_token',
     expireTime: 'spotify_token_expire_time',
     timestamp: 'spotify_token_timestamp',
-
-};
-
-// Map to retrieve localStorage values
-const LOCALSTORAGE_VALUES = {
+  }
+  
+  // Map to retrieve localStorage values
+  const LOCALSTORAGE_VALUES = {
     accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
     refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
     expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
     timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
-
-};
-
-
+  };
 
 
 /**
  * Handles logic for retrieving the Spotify access token from localStorage
  * or URL query params
- * @returns {string} A spotify access token
+ * @returns {string} A Spotify access token
  */
-const getAccessToken = () => {
-    const queyrString = window.location.search;
-    const urlParams = new URLSearchParams(queyrString);
+ const getAccessToken = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
     const queryParams = {
-        [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
-        [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('refresh_token'),
-        [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('expires_in'),
+      [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
+      [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
+      [LOCALSTORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
     };
-
     const hasError = urlParams.get('error');
-    
+  
     // If there's an error OR the token in localStorage has expired, refresh the token
     if (hasError || hasTokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined') {
-        refreshToken();
+      refreshToken();
     }
-
+  
     // If there is a valid access token in localStorage, use that
     if (LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined') {
-        return LOCALSTORAGE_VALUES.accessToken;
+      return LOCALSTORAGE_VALUES.accessToken;
     }
-
+  
     // If there is a token in the URL query params, user is logging in for the first time
     if (queryParams[LOCALSTORAGE_KEYS.accessToken]) {
-        // Store the query params in localStorage
-        for (const property in queryParams) {
+      // Store the query params in localStorage
+      for (const property in queryParams) {
         window.localStorage.setItem(property, queryParams[property]);
-        }
-        // Set timestamp
-        window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
-        // Return access token from query params
-        return queryParams[LOCALSTORAGE_KEYS.accessToken];
+      }
+      // Set timestamp
+      window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+      // Return access token from query params
+      return queryParams[LOCALSTORAGE_KEYS.accessToken];
     }
-
+  
     // We should never get here!
     return false;
-};
+  };
 
 
 
-
-/**
+  /**
  * Clear out all localStorage items we've set and reload the page
  * @returns {void}
  */
- export const logout = () => {
+export const logout = () => {// Map for localStorage keys
+    const LOCALSTORAGE_KEYS = {
+      accessToken: 'spotify_access_token',
+      refreshToken: 'spotify_refresh_token',
+      expireTime: 'spotify_token_expire_time',
+      timestamp: 'spotify_token_timestamp',
+    }
+    
+    // Map to retrieve localStorage values
+    const LOCALSTORAGE_VALUES = {
+      accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+      refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+      expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+      timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+    };
     // Clear all localStorage items
     for (const property in LOCALSTORAGE_KEYS) {
       window.localStorage.removeItem(LOCALSTORAGE_KEYS[property]);
@@ -78,27 +86,14 @@ const getAccessToken = () => {
     // Navigate to homepage
     window.location = window.location.origin;
   };
-/**
- * Checks if the amount of time that has elapsed between the timestamp in localStorage
- * and now is greater than the expiration time of 3600 seconds (1 hour).
- * @returns {boolean} Whether or not the access token in localStorage has expired
- */
- const hasTokenExpired = () => {
-    const { accessToken, timestamp, expireTime } = LOCALSTORAGE_VALUES;
-    if (!accessToken || !timestamp) {
-      return false;
-    }
-    const millisecondsElapsed = Date.now() - Number(timestamp);
-    return (millisecondsElapsed / 1000) > Number(expireTime);
-  };
 
 
-/**
+  /**
  * Use the refresh token in localStorage to hit the /refresh_token endpoint
  * in our Node app, then update values in localStorage with data from response.
  * @returns {void}
  */
- const refreshToken = async () => {
+const refreshToken = async () => {
     try {
       // Logout if there's no refresh token stored or we've managed to get into a reload infinite loop
       if (!LOCALSTORAGE_VALUES.refreshToken ||
@@ -124,4 +119,19 @@ const getAccessToken = () => {
     }
   };
 
-export const accessToken = getAccessToken();
+  /**
+ * Checks if the amount of time that has elapsed between the timestamp in localStorage
+ * and now is greater than the expiration time of 3600 seconds (1 hour).
+ * @returns {boolean} Whether or not the access token in localStorage has expired
+ */
+const hasTokenExpired = () => {
+    const { accessToken, timestamp, expireTime } = LOCALSTORAGE_VALUES;
+    if (!accessToken || !timestamp) {
+      return false;
+    }
+    const millisecondsElapsed = Date.now() - Number(timestamp);
+    return (millisecondsElapsed / 1000) > Number(expireTime);
+  };
+
+
+  export const accessToken = getAccessToken();
