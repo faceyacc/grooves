6e0ecebceb82b26385b439e905650625abec285b
Ty <justfacey@gmail.com>
2022-11-22 18:32:51 -0800

    added logout button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import {accessToken} from "./spotify";
-import { useEffect, useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import { accessToken, logout } from './spotify';
 import './App.css';
 
 function App() {
-
   const [token, setToken] = useState(null);
 
   useEffect(() => {
@@ -14,15 +12,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {
-          !token ? (
-            <a className="App-link" href="http://localhost:8888/login">
-              Log in to Spotify
-            </a>
-          ) : (
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
             <h1>Logged in!</h1>
-          )
-        }
+            <button onClick={logout}>Log Out</button>
+          </>
+        )}
       </header>
     </div>
   );
