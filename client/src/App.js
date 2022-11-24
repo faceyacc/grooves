d8f8a889127175e2efa436952b3f99d6772fdfa7
Ty <justfacey@gmail.com>
2022-11-23 18:11:09 -0800

    added routes to handlers

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,42 @@ import { useState, useEffect } from 'react';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import './App.css';
 import { catchErrors } from './utils';
+import {BrowserRouter as Router,
+        Routes,
+        useLocation,
+        Route
+      } from "react-router-dom";
+
+
+// Returns home page 
+function home(profile) {
+  return(
+    <>
+    <button onClick={logout}>Log Out</button>
+
+    {profile && (
+      <div>
+        <h1>{profile.display_name}</h1>
+        <p>{profile.followers.total} Followers</p>
+        {profile.images.length && profile.images[0].url && (
+          <img src={profile.images[0].url} alt="Avatar"/>
+        )}
+      </div>
+    )}
+  </>
+  )
+};
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname]);
+}
+
 
 function App() {
   const [token, setToken] = useState(null);
@@ -27,19 +63,25 @@ function App() {
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
+          <Router>
+            <ScrollToTop />
+            <Routes>
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar"/>
-                )}
-              </div>
-            )}
-          </>
+              <Route path="/top-artists" element={<h1>Top Artist</h1>}></Route>
+
+              <Route path="/top-tracks" element={<h1>Top Tracks</h1>}></Route>
+
+              <Route path="/playlists/:id" element={<h1>Playlist</h1>}></Route>
+
+              <Route path="/playlists/" element={<h1>Playlist</h1>}></Route>
+
+              <Route path="/" element={home(profile)}>
+
+              </Route>
+            </Routes>
+
+            
+          </Router>
         )}
       </header>
     </div>
